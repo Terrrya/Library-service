b202ce5d741dda6625cd158be23ea7860e746fc1
Denys Tereshchuk <terrya@ukr.net>
2023-03-15 16:06:59 +0400

    Added permissions to Book service endpoints

--- book/views.py
+++ book/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from book.models import Book
+from book.permissions import IsAdminOrAnyReadOnly
 from book.serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAdminOrAnyReadOnly,)
