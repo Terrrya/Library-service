778bffa53bca7bb2aba7198a925a3d3b92f53145
Denys Tereshchuk <terrya@ukr.net>
2023-03-16 14:39:26 +0400

    Added BorrowListRetrieveViewSet

--- borrow/views.py
+++ borrow/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from borrow.models import Borrow
+from borrow.serializers import BorrowSerializer, BorrowListSerializer
+
+
+class BorrowListRetrieveViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Borrow.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowListSerializer
+        if self.action == "retrieve":
+            return BorrowListSerializer
+        return BorrowSerializer
