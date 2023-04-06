8402ac03fa49faf765b3d6e1ee14fc2614b343a8
Denys Tereshchuk <terrya@ukr.net>
2023-04-07 00:25:05 +0400

    Updated swagger documentation for user endpoint

--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
@@ -6,11 +7,20 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     """Create new user"""
+
     serializer_class = UserSerializer
 
 
+@extend_schema_view(
+    get=extend_schema(description="Return logged-in user information"),
+    put=extend_schema(description="Update logged-in user information"),
+    patch=extend_schema(
+        description="Partial update logged-in user information"
+    ),
+)
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Update user witch already login"""
+
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
