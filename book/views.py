b922063e6faa629b4e3e568eec2bfa3aa344f76d
Denys Tereshchuk <terrya@ukr.net>
2023-04-07 00:24:58 +0400

    Updated swagger documentation for books endpoint

--- book/views.py
+++ book/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets
 
 from book.models import Book
@@ -5,6 +6,22 @@ from book.permissions import IsAdminOrAnyReadOnly
 from book.serializers import BookSerializer
 
 
+@extend_schema_view(
+    create=extend_schema(
+        description="Crate new book. Only staff user can create"
+    ),
+    list=extend_schema(description="Return list of all books"),
+    retrieve=extend_schema(description="Return book detail information"),
+    update=extend_schema(
+        description="Update book. Only staff user can update"
+    ),
+    partial_update=extend_schema(
+        description="Partial update book. Only staff user can partial update"
+    ),
+    destroy=extend_schema(
+        description="Delete book. Only staff user can delete"
+    ),
+)
 class BookViewSet(viewsets.ModelViewSet):
     """Book CRUD endpoints"""
 
