a6c6f9fbdc0ee4c9aef4c77ba9fc7121d9ec7f71
Denys Tereshchuk <terrya@ukr.net>
2023-04-20 14:20:17 +0400

    Updated test_book_api

--- tests/test_book_api.py
+++ tests/test_book_api.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -17,7 +18,7 @@ def sample_book(**params) -> Book:
         "author": "Test Author",
         "cover": Book.CoverChoices.HARD,
         "inventory": 25,
-        "daily_fee": Decimal(1.45),
+        "daily_fee": 1.45,
     }
     default.update(params)
     return Book.objects.create(**default)
@@ -33,12 +34,12 @@ class UnauthenticatedBookApiTests(TestCase):
 
     def test_list_book(self) -> None:
         sample_book()
-        books = Book.objects.all()
+        books = Book.objects.all()[:10]
 
         response = self.client.get(BOOK_URL)
         serializer = BookSerializer(books, many=True)
-
-        self.assertEqual(response.data, serializer.data)
+        print(serializer.data)
+        self.assertEqual(response.data["results"], serializer.data)
 
     def test_retrieve_book(self) -> None:
         book = sample_book()
@@ -55,7 +56,7 @@ class UnauthenticatedBookApiTests(TestCase):
             "author": "Test Author",
             "cover": Book.CoverChoices.HARD,
             "inventory": 25,
-            "daily_fee": Decimal(1.45),
+            "daily_fee": 1.45,
         }
 
         response = self.client.post(BOOK_URL, data=payload)
@@ -68,7 +69,7 @@ class UnauthenticatedBookApiTests(TestCase):
             "author": "Test2 Author",
             "cover": Book.CoverChoices.SOFT,
             "inventory": 125,
-            "daily_fee": Decimal(1.00),
+            "daily_fee": 1.00,
         }
         book = sample_book()
 
@@ -93,3 +94,133 @@ class UnauthenticatedBookApiTests(TestCase):
         response = self.client.delete(detail_book_url(book.id))
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedBookApiTests(UnauthenticatedBookApiTests):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            email="test@test.com", password="test12345"
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_create_book_not_allowed(self) -> None:
+        payload = {
+            "title": "Test Book",
+            "author": "Test Author",
+            "cover": Book.CoverChoices.HARD,
+            "inventory": 25,
+            "daily_fee": 1.45,
+        }
+
+        response = self.client.post(BOOK_URL, data=payload)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_book_not_allowed(self) -> None:
+        payload = {
+            "title": "Test2 Book",
+            "author": "Test2 Author",
+            "cover": Book.CoverChoices.SOFT,
+            "inventory": 125,
+            "daily_fee": 1.00,
+        }
+        book = sample_book()
+
+        response = self.client.put(detail_book_url(book.id), payload)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_partial_update_book_not_allowed(self) -> None:
+        payload = {
+            "title": "New Test Book",
+            "inventory": 225,
+        }
+        book = sample_book()
+
+        response = self.client.patch(detail_book_url(book.id), payload)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_book_not_allowed(self) -> None:
+        book = sample_book()
+
+        response = self.client.delete(detail_book_url(book.id))
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminBookApiTests(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            email="test@test.com",
+            password="test12345",
+            is_staff=True,
+            is_superuser=True,
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_list_book(self) -> None:
+        sample_book()
+        books = Book.objects.all()[:10]
+
+        response = self.client.get(BOOK_URL)
+        serializer = BookSerializer(books, many=True)
+        print(serializer.data)
+        self.assertEqual(response.data["results"], serializer.data)
+
+    def test_retrieve_book(self) -> None:
+        book = sample_book()
+
+        response = self.client.get(detail_book_url(book.id))
+        serializer = BookSerializer(book)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_book(self) -> None:
+        payload = {
+            "title": "Test Book",
+            "author": "Test Author",
+            "cover": Book.CoverChoices.HARD,
+            "inventory": 25,
+            "daily_fee": 1.45,
+        }
+
+        response = self.client.post(BOOK_URL, payload)
+        print(response.data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_update_book(self) -> None:
+        payload = {
+            "title": "Test2 Book",
+            "author": "Test2 Author",
+            "cover": Book.CoverChoices.SOFT,
+            "inventory": 125,
+            "daily_fee": 1.00,
+        }
+        book = sample_book()
+
+        response = self.client.put(detail_book_url(book.id), payload)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_book(self) -> None:
+        payload = {
+            "title": "New Test Book",
+            "inventory": 225,
+        }
+        book = sample_book()
+
+        response = self.client.patch(detail_book_url(book.id), payload)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_book(self) -> None:
+        book = sample_book()
+
+        response = self.client.delete(detail_book_url(book.id))
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
